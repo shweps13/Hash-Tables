908b49d2920ffb07367383fb5d88c6bebf6dedff
Heorhii Siburov <g.siburov@gmail.com>
2020-02-10 11:02:46 -0800

    class-work

--- hashes/hashes.py
+++ hashes/hashes.py
@@ -1,2 +1,15 @@
 import hashlib
 
+key = b"str2" #byte string?
+my_string = "just a string".encode()
+
+# for i in range(10):
+#     hashed = hashlib.sha256(key).hexdigest()
+#     print(hashed)
+    # hashed = hashlib.sha256(key)
+    # breakpoint()
+
+for i in range(10):
+    hashed = hash(key)
+    print(hashed % 8)
+
